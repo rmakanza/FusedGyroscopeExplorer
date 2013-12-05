24996de995b4c1f763e9ad7615d2673e104be096
Kaleb <kaleb.kircher@gmail.com>
2013-12-05 15:06:45 -0700

    Update to license.

--- FusedGyroscopeExplorer/src/com/kircherelectronics/fusedgyroscopeexplorer/sensor/FusedGyroscopeSensor.java
+++ FusedGyroscopeExplorer/src/com/kircherelectronics/fusedgyroscopeexplorer/sensor/FusedGyroscopeSensor.java
@@ -1,5 +1,44 @@
 package com.kircherelectronics.fusedgyroscopeexplorer.sensor;
 
+/*
+ * Copyright 2013, Kaleb Kircher - Boki Software, Kircher Electronics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2012 Paul Lawitzki
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import java.util.ArrayList;
 
 import android.annotation.TargetApi;
@@ -35,6 +74,14 @@ import com.kircherelectronics.fusedgyroscopeexplorer.sensor.observer.MagneticSen
  * intervals. This is equivalent to low-pass filtering of the accelerometer and
  * magnetic field sensor signals and high-pass filtering of the gyroscope
  * signals.
+ * 
+ * Note: The fusion algorithm itself was written by Paul @
+ * http://www.thousand-thoughts.com/2012/03/android-sensor-fusion-tutorial/ and
+ * taken from his SensorFusion1.zip project. J.W. Alexandar Qiu has credit for
+ * the transitions between 179° <–> -179° fix. I have optimized some of the code
+ * and made it slightly easier to follow and read. I have also changed the
+ * SensorManager.getRotationMatrix() to use the gravity sensor instead of the
+ * acceleration sensor.
  * 
  * @author Kaleb
  * @version %I%, %G%
